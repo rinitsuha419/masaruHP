f65bf1383a272f74320f9a3d6ae54a2c98704df7
rinitsuha419 <s1f102400639@iniad.org>
2024-09-02 02:42:20 +0900

    基本

---
+++ first.js
@@ -0,0 +1,16 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.transform = `translateX(${100 * (i - index)}%)`;
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+// スライドの遷移間隔を10秒に設定
+setInterval(nextSlide, 10000); // 10秒ごとにスライド
